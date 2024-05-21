cc3c9c255d795509ceda41722b98c95f0924f945
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-05-21 17:22:29 +0530

    update

---
+++ simple-frist-program/non-blocking-exmaple.js
@@ -0,0 +1,11 @@
+// creating the example of the non - blocking 
+
+// creating the settime out function 
+setTimeout(()=>{
+    console.log('this is the inner the loop function ') ; 
+},8000) ; 
+
+// creating the console function to print and the display the function 
+console.log("this is the printing the after the loop over ") ; 
+
+// this is the printing the loop like the over 
