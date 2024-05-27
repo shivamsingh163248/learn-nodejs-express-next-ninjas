e487271fb80a2dfb0c4295f6ca92c78964af7041
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-05-27 19:41:27 +0530

    update

---
+++ learning-creating-server/multipale-request-handale.js
@@ -0,0 +1,13 @@
+// now learning the multiple request handale in the js 
+// how the server the handale the multiple request 
+const http = require('http') ; 
+
+// creating the instance of the server of the node \
+const server = http.createServer((req, res)=>{
+
+    // there are the two variable in the node 
+    res.end("this is the givne the responve of the server ") ; 
+
+})
+
+// define the post number 
