1c3eddf94e2b62560f3d6b9eb9df8519afb78069
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-05-27 11:17:19 +0530

    update

--- learning-creating-server/server.js
+++ learning-creating-server/server.js
@@ -6,3 +6,12 @@
   const http = require('http') ; 
   
   // now https have the method  
+ const server =   http.createServer((req , repo) => {
+    repo.end("this is the my server");
+
+   })
+
+   // step 2 in this the Liston in the liston the port number 
+   server.listen(3100,()=>{
+    console.log("now server is the getting the started") ;
+   })
