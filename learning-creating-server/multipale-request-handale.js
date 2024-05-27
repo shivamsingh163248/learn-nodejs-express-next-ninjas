06e942c593e614f8b1e82ffcebaaa788ee626021
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-05-27 22:08:52 +0530

    update the js coded for the handle the multiple request

--- learning-creating-server/multipale-request-handale.js
+++ learning-creating-server/multipale-request-handale.js
@@ -8,7 +8,13 @@ const server = http.createServer((req, res)=>{
     // there are the two variable in the node
     // now printing on the console
     // creating the multiple request 
-     
+    if(req.url == "/user"){
+        // write the server 
+        res.end("this is the my user request ") ; 
+        // also using the write method 
+        res.write("this is write method are the using ") ; 
+    }
+
     console.log(req.url) ; 
     
     res.end("this is the givne the responve of the server ") ; 
