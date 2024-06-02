b15090b975b48ccbf112f2d2d16884c83f73dffa
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-03 02:50:59 +0530

    update

--- learning-creating-server/handal-Multiple-Responce.js
+++ learning-creating-server/handal-Multiple-Responce.js
@@ -1,10 +1,10 @@
 // creating the backend for the node js 
 
 // for the accepting the http 
-const server =   require('http') ;
+const http =   require('http') ;
 
 // now creating the instance of the server 
-server.createServer((req , res)=>{
+const server = http.createServer((req , res)=>{
 
     // in the response part to 
     res.write("this is the my server my name is shivam singh ") ; 
@@ -12,8 +12,19 @@ server.createServer((req , res)=>{
    // creating the condition to handle to multiple url 
    if (req.url == "/payment") {
     
-    
+    return res.end("this is the payment method ") ; 
+
+   }else if (req.url == "/user") {
+    return res.end("this is the user services ") ; 
+
    }
 
 
+})
+
+
+// creating the listion port 
+
+server.listen(3100 , ()=>{
+    console.log("the server is the listen of the port on the 3100") ; 
 })
