4431853dce1f4fec6525dd22f0efb1c10542d112
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-05 11:47:50 +0530

    update

---
+++ learning-module-in-js-nodejs/js-moduke.js
@@ -0,0 +1,14 @@
+// learning the module 
+
+// creating the two function 
+function sum(a , b){
+    return a+b ; 
+
+}
+
+function multiply(a,b){
+    return a*b ; 
+}
+
+
+// two way to creating the module to use this file in the another file 
