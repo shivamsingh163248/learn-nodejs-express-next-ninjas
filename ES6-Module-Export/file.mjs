5fb07e1bd4eba91e021871659064febee39a4da8
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-07 22:31:17 +0530

    update

---
+++ ES6-Module-Export/file.mjs
@@ -0,0 +1,24 @@
+
+// hear creating the file that are the using the app in the js 
+export function sum(a,b){
+    return a+b ; 
+}
+ const age = (a,b)=>{
+    return a*b ; 
+}
+
+// creating the object 
+
+export const first  = {
+    class : "b.tech ",
+    rollNo : 34545 
+
+}
+
+
+// now exporting 
+
+export {age} ; 
+
+
+
