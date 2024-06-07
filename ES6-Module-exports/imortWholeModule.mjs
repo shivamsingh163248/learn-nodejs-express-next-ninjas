ae6af77de31ee14af911c5a2c14d9067fea6b450
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-07 09:35:38 +0530

    update the es6 module

---
+++ ES6-Module-exports/imortWholeModule.mjs
@@ -0,0 +1,17 @@
+
+
+// using the * as the from name of the path to importing the module 
+
+import * as check from "./arithmatic.mjs"
+
+// hear we are the importing the the whole module the using the check as the object 
+
+// using the function 
+const ans = check.multi(6,6) ;
+console.log(ans) ;  
+// using the object
+
+const obj = check.details ; 
+
+console.log(obj.name) ; 
+
---
+++ ES6-Module-exports/module.mjs
@@ -0,0 +1,19 @@
+
+// creating the learning the module to learn 
+
+
+// if we are the importing the single module 
+
+import { details } from "./arithmatic.mjs";
+
+// print all the object name 
+
+for (const key in details) {
+    console.log(key) ; 
+
+}
+
+// importing the variavle 
+import { name } from "./arithmatic.mjs";
+console.log(name) ; 
+
