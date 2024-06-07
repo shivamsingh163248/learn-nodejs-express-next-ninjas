758df821640e16831d42dfa18d86990bed4ad4e2
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-06-07 09:03:12 +0530

    learning the Es6 module

--- ES6-Module-exports/arithmatic.mjs
+++ ES6-Module-exports/arithmatic.mjs
@@ -5,3 +5,17 @@ export function sum(a,b){
 return a+b ; 
 }
 // and exporting the variable 
+export let name = "shivam singh " ; 
+
+export function multi(a,c){
+    return a*c ; 
+}
+
+
+// export are the using  to export are the using object 
+export  const details  = {
+    name  : " shivam singh " , 
+    fatherName : "satyendra kumar singh ", 
+    MotherName : "sarita singh ",
+    
+}
