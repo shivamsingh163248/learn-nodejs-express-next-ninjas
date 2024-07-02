6b209f66af0560e73a711467078835ee3acd4bcf
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-02 10:29:00 +0530

    update

---
+++ Module/ES6/type1.mjs
@@ -0,0 +1,11 @@
+// creating the and the learning 
+
+export function add(a,b){
+    return a+b ; 
+}
+
+// we forgot to declerd the variable 
+export const obj = {
+    name : "shivam singh " , 
+    age : 454
+}
---
+++ Module/ES6/type1i.mjs
@@ -0,0 +1,10 @@
+
+// three type of importing available 
+
+import * as ans from './type1.mjs' ;  // importing for the full line 
+
+import {add , obj} from './type1.mjs' ; // importing particular element  
+
+console.log(ans.add(4,6)) ; 
+console.log(obj) ; 
+
