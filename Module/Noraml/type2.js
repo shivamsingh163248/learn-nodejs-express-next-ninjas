62b47d0ac1a8966e6c8f7ebeb3277a479e42ecb7
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-02 10:03:28 +0530

    update

---
+++ Module/Noraml/type2.js
@@ -0,0 +1,18 @@
+// creating the exporting using the 
+
+// exporting the property 
+
+module.exports.sum = (a,b)=>{
+    return a+b ; 
+}
+exports.mins = (a,b)=>{
+    return a-b ; 
+}
+
+// creating the exporting the function 
+
+// this is the type 3 of the creating the module and the exporting the module 
+
+module.exports = function(a,b){
+    return a+b ; 
+}
---
+++ Module/Noraml/type2i.js
@@ -0,0 +1,5 @@
+
+const ans = require('./type2')
+
+// console.log(ans.sum(4,6)) ;  // this is the also working 
+console.log(ans(4,6)) ; 
