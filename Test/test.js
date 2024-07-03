44022544b2828313a72210a6ac0f1dd9a6e9e9a9
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-03 22:09:12 +0530

    learning the readline

--- Test/test.js
+++ Test/test.js
@@ -1,1 +1,14 @@
-const ans = require('readline')
+const { stdin, stdout } = require('process');
+const ans = require('readline')  ; 
+const value = ans.Interface({
+    input : stdin ,
+    output : stdout
+})
+
+
+ value.question('enter your name ' , (name)=>{
+      
+    value.question('enter your age' , (age)=>{
+        console.log(name+" "+age ) ; 
+    })
+ })
