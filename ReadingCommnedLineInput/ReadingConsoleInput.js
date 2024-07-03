e8ca2f0758e5d3780e11150980e48d776343d694
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-03 16:24:32 +0530

    update values

--- ReadingCommnedLineInput/ReadingConsoleInput.js
+++ ReadingCommnedLineInput/ReadingConsoleInput.js
@@ -1,1 +1,17 @@
-// learning the input form the console using the module readline that are using the read console 
+// learning the input form the console using the module readline that are using the read console
+
+const readline = require("readline") ;
+
+//creating interface for the readline
+const interface =  readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+ });
+
+ interface.question("what is your name ?" , (name) => {
+     console.log("your name is " + name) ;
+     interface.close() ;
+ }) ;
+
+
+ 
