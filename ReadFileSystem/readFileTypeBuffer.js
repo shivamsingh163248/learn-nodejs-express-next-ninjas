a4a07a3cd6fd2d4b504fdc99685ac690d6018645
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-04 17:10:15 +0530

    update

---
+++ ReadFileSystem/readFileTypeBuffer.js
@@ -0,0 +1,5 @@
+// creating and the learning the using the buffer without the encoding using the buffer.string to read file 
+const fs = require('fs') ; 
+
+// creating the sync function for the reading the file 
+  var data = fs.readFileSync('itisha.text');
