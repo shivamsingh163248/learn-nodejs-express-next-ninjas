39f44286ddd42c0ea716a529dc038c9edaf1aca5
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-04 20:18:18 +0530

    update

---
+++ WriteFileSystem/writeFilefs.js
@@ -0,0 +1,10 @@
+
+// using the fs module   we are writing file and learning the append the file 
+const fs = require('fs') ; 
+
+// creating the writeFileSync using the write file 
+const value = fs.writeFileSync('WriteFileSystem/shivam.text' , "this is the name : shivam singh ") ;  
+
+// creating the all the file 
+
+// creating the file and append 
