ed62340112baf427c2846be7f8aa440990672e3b
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-05 12:48:48 +0530

    update

--- FileModuleNonBlocking/ReadFileModule.js
+++ FileModuleNonBlocking/ReadFileModule.js
@@ -1,3 +1,12 @@
 // now creating the read file module 
 const fs = require('fs') ; 
- 
+ // creating the file using the read there are the three parameter 
+fs.readFile('FileModuleNonBlocking/shiv.txt' ,(err,data)=>{
+    console.log('this is the data that we are the append ') ; 
+    console.log(data.toString()) ; 
+    try {
+        console.log('this is not error ') ; 
+    } catch (error) {
+        console.log(err) ; 
+    }
+}) 
