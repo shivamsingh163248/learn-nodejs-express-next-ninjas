f2e8a63352736af160c85ae292c0d2b4426b5e5b
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-08 20:03:01 +0530

    update

---
+++ NodeMailer/Mail.js
@@ -0,0 +1,70 @@
+
+// step 1 
+const nodemailer =  require('nodemailer');
+
+ 
+// nodemailer to creating the transport for the using for the transport 
+
+
+
+// var transporter = nodemailer.createTransport({
+//     service: 'smtp.hostinger.com',
+//     port: 465,
+//     secure: true,
+//     auth: {
+//       user: 'support@lovelymobilecenter.store',
+//       pass: 'Itisha@2550'
+//     }
+//   });
+
+// var mailOptions = {
+//     from: 'support@lovelymobilecenter.store',
+//     to: 'shivamsingh163248@gmail.com',
+//     subject: 'Sending Email using Node.js',
+//     text: 'That was easy!'
+//   };
+  
+//   transporter.sendMail(mailOptions, function(error, info){
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log('Email sent: ' + info.response);
+//     }
+//   });
+
+// creating the function of the sand mail 
+async function sandMail(){
+    
+ // using the nodemailer 
+var transporter = nodemailer.createTransport({
+    host: 'smtp.hostinger.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'support@lovelymobilecenter.store',
+      pass: 'Itisha@2550'
+    }
+  });
+  var mailOptions = {
+    from: 'support@lovelymobilecenter.store',
+    to: 'shivamsingh163248@gmail.com', 
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+
+  // in the try catch we are the using for the sanding mail 
+  try {
+ const result  = await  transporter.sendMail(mailOptions) ; 
+ console.log(result) ; 
+ console.log("sand mail successful ")  
+  } catch (error) {
+    console.log(`this is the error message ` + error) ; 
+  }
+
+
+
+}
+
+// using the 
+sandMail()  ; 
+
