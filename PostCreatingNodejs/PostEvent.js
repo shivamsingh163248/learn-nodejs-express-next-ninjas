2cfe20e830ef916b46361d1a5e631dc2e45e0c2d
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-09 13:17:17 +0530

    update

---
+++ PostCreatingNodejs/PostEvent.js
@@ -0,0 +1,13 @@
+// now learning the post event 
+// fist we are the creating the server 
+
+const http = require('http') ;
+
+const server = http.createServer((req , res)=>{
+
+  
+
+})
+
+// hear creating the list 
+
