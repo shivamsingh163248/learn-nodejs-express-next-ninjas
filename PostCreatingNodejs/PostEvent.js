ace857df3dfa29931a6638b8ccbf6d7c4f3a0f0e
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-13 22:45:52 +0530

    update

--- PostCreatingNodejs/PostEvent.js
+++ PostCreatingNodejs/PostEvent.js
@@ -7,6 +7,20 @@ const server = http.createServer((req , res)=>{
 
     if (req.method == POST) {
         
+        let body = '' ; 
+
+        req.on('data' , (chunk)=>{
+            body += chunk.toString() ; 
+
+        })
+
+        // again we are the writing the chung 
+        req.on('end' , ()=>{
+            console.log(body) ; 
+            // and printing the data successful 
+            console.log('data are the successful stored ')  ; 
+            res.end('data revived successful ') ; 
+        })
     }
         
     res.end('this is the end of the ') ; 
