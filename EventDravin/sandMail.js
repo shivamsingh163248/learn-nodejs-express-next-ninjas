fc5e17beed5311c49caf8722ebd7cd41a8cc2f91
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-19 20:13:17 +0530

    updaste

--- EventDravin/sandMail.js
+++ EventDravin/sandMail.js
@@ -21,13 +21,48 @@ const FirstEvent = new event() ;
 
 // creating the server using the http module that are using to creating the server 
 const https = http.createServer((req , res)=>{
+    
+    // using the node mailer using the node 
+    // nodemailer is the extension of the sanding the mail this is the using the npm install nodemailer 
+    // that is the name of the transporter  
+   const transporter  =  nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "codingninjas2k16@gmail.com",
+            pass: "slwvvlczduktvhdj",
+          }, 
+
+    });
+
+
 
 // we are the handling server 
 if (req.method == 'POST') {
     
   if (req.url = '/mailSand') {
     
-       
+      
+    // creating the nodemailer 
+    let value = '' ; 
+    req.on('data' , (chunk)=>{
+
+        value += chunk ; 
+    })
+
+    // now we are the using the end method 
+
+    req.on('end' , ()=>{
+
+        // we are the changing the value in the jason form 
+        const jasonData = JSON.parse(value) ; 
+         
+        // now we are the printing 
+        // we are the spiting the object 
+        
+    
+    })
+
+
 
   }
 
