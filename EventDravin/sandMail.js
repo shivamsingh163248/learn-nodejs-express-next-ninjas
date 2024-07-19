3c41b2427efaf2e6b60b7277c25b63ecfff1f7a9
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-19 18:54:14 +0530

    learning the event module

--- EventDravin/sandMail.js
+++ EventDravin/sandMail.js
@@ -1,1 +1,22 @@
 // creating and the importing all the 
+import http  from 'http'
+import fs from 'fs'
+import nodemailer from 'nodemailer'
+import Event from 'events'
+
+// creating the import the event hear is the event emitter 
+
+
+// creating the class for the event 
+class event extends Event.EventEmitter{
+    // creating the function after the calling the event emitter 
+    sandMail(email){
+        this.emit('sandMail' , email) ; 
+    }
+}
+
+// now creating the class 
+
+const FirstEvent = new event() ; 
+
+
