c8b2569f0f0d1d8e20a86defc1fff92fe51be1d2
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-21 07:10:47 +0530

    leanring the server.use methond and learning the handeling the default request

--- Express/UseMethodMiddleWare/useFunction.js
+++ Express/UseMethodMiddleWare/useFunction.js
@@ -1,2 +1,12 @@
 // now we are the handle the using the middle ware in the express 
 // learning the use function to excite any path 
+const express = require('express') ; 
+
+// now creating the server 
+const server = express() ; 
+
+// now creating the function 
+
+function sandMessage(req , res , next){
+  console.log('this is the function of the sand message ') ; 
+}
