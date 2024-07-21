12d9a5b10bbea487f6aa41ed5ee7ce348fecd293
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-21 19:10:45 +0530

    update

---
+++ Express/expressTypeRequest/requestType.js
@@ -0,0 +1,22 @@
+// now we are the learning the request type 
+const express = require('express') 
+
+// creating the function 
+const server = express() ; 
+
+
+// now creating the type of the request 
+server.get('/' , (req , res)=>{
+
+}) 
+
+server.post('/' , (req , res)=>{
+
+})
+server.put('/' , (req , res)=>{
+
+})
+
+server.delete('/' , (req , res)=>{
+    
+})
