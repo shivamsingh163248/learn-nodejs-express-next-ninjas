eb474c816b4692b85688d288ec87c36975922cbc
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-21 09:26:09 +0530

    handeling the defalut request

--- Express/UseMethodMiddleWare/useFunction.js
+++ Express/UseMethodMiddleWare/useFunction.js
@@ -9,4 +9,27 @@ const server = express() ;
 
 function sandMessage(req , res , next){
   console.log('this is the function of the sand message ') ; 
+  next() ; 
 }
+
+// now creating the second message 
+function sandMail(req , res , next){
+  console.log('this is the function for the sanding the mail') ; 
+  next() ; 
+}
+
+function callingApi(req , res , next){
+  console.log('this is the calling the Api') ; 
+  next() ; 
+}
+
+// now creating get request 
+server.get('/' , sandMessage , sandMail , callingApi , (req ,res)=>{
+  res.send('now this is the the response ') ; 
+})
+
+
+// now creating the server liston function 
+server.listen(3100 , ()=>{
+  console.log('now server are the getting started ') ; 
+})
