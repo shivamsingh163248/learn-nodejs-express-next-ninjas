b41378079a308188061ba13df5a1800017dbd1c5
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-21 19:17:06 +0530

    update the code

--- Express/expressTypeRequest/requestType.js
+++ Express/expressTypeRequest/requestType.js
@@ -7,16 +7,26 @@ const server = express() ;
 
 // now creating the type of the request 
 server.get('/' , (req , res)=>{
-
+   
+    res.send('this is the get server ') ; 
 }) 
 
 server.post('/' , (req , res)=>{
+    console.log('this is the post request') ; 
 
 })
 server.put('/' , (req , res)=>{
-
+  console.log('this is the put request') ; 
 })
 
 server.delete('/' , (req , res)=>{
-    
+    console.log('this is the deleted request') ; 
+
+}) 
+
+// now creating the for the server for the liston 
+
+server.listen(5100 , ()=>{
+    console.log('now server are the getting started ') ; 
+
 })
