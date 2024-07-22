59e44f801f9f4bb235dbe2f7c089f3a8ee039952
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-22 15:31:43 +0530

    update

--- Express/ServeringHtmlFile/UsingHttp.js
+++ Express/ServeringHtmlFile/UsingHttp.js
@@ -1,19 +1,22 @@
 
 const http = require('http') ;
 const fs = require('fs');
+ 
 
 // now creating the sever 
 
-const server = http.createServer((req , res)=>{
+const server = http.createServer( (req , res)=>{
 
 // now creating the fs module to read file 
 // creating the fs module to read file 
-const file = fs.readdirSync('./public/index.html').toString() ; 
+
+const file = fs.readFileSync("./Express/ServeringHtmlFile/public/index.html").toString() ; 
 // now ending the response 
 res.end(file) ; 
-
 })
 
 
 // now creating the for the server liston
-
+server.listen(5100 , ()=>{
+   console.log('server are the getting started ') ; 
+})
