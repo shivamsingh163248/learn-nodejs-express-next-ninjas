6cd42ecea5fe427b4e252074ca0ef4bd03e1bf70
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-22 17:02:54 +0530

    update

--- Express/ServeringHtmlFile/UsingExpress.js
+++ Express/ServeringHtmlFile/UsingExpress.js
@@ -1,2 +1,20 @@
+const express = require('express') ; 
+const fs  = require('fs') ; 
 
-// 
+
+// now creating the server 
+const server = express() ; 
+
+
+server.use( express.static('./Express/ServeringHtmlFile/public')  ) ; // checking the directory  
+server.get('/' , (req , res)=>{
+   
+    // now creating the for the the response
+    res.send('this is the successful') ; 
+})
+
+server.listen(5100 , ()=>{
+    console.log('now server are the getting started') ; 
+})
+
+//
