da51802d3e7867f303a0f742187dcdb251f72358
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-23 15:57:20 +0530

    update

--- Project/MVC-Inventory-Management/index.js
+++ Project/MVC-Inventory-Management/index.js
@@ -1,9 +1,26 @@
 // creating the server using the express 
 const express = require('express') ; 
 const fs = require('fs') ; 
+const path = require('path') ; 
 
 
 // creating the server 
+const server = express() ; 
 
 // now creating the server using the get 
+server.use(express.static('./Project/MVC-Inventory-Management/src/view')) ; 
+
+
+
+
+server.get('/', (req , res)=>{
+
+    res.send('this is the fist response') ; 
+
+})
+
+// now creating the server liston 
+server.listen(5100 , ()=>{
+    console.log('now server is the getting started ') ; 
+})
 
