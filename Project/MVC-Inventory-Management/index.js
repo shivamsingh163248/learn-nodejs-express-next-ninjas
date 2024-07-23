dd64c35cf445a4c8a23999b85c0b38f99ea5bc20
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-23 10:24:22 +0530

    learning the invotory project

---
+++ Project/MVC-Inventory-Management/index.js
@@ -0,0 +1,9 @@
+// creating the server using the express 
+const express = require('express') ; 
+const fs = require('fs') ; 
+
+
+// creating the server 
+
+// now creating the server using the get 
+
