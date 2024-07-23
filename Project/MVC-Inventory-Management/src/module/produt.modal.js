6e0ecf3350f5751e3e4f0c84186e432e7482abe8
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-23 20:02:19 +0530

    update

---
+++ Project/MVC-Inventory-Management/src/module/produt.modal.js
@@ -0,0 +1,10 @@
+// creating the product modal are the using 
+// creating the function 
+import path from "path";
+
+// creating the class 
+export default class productModule{
+
+    // creating the contactor 
+    
+}
