04e11d7fa85a359bc702203a8a5358cf9b573cc1
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-30 22:18:18 +0530

    update

--- Project/MVC-Inventory-Management/src/controler/product.contoler.js
+++ Project/MVC-Inventory-Management/src/controler/product.contoler.js
@@ -1,12 +1,17 @@
 // creating the object of the controller 
 
 import path from "path";
+import productModule from "../module/produt.modal";
 
 // creating the class 
 
  export default class product {
 
 // creating the function of th product controller
+// using the static method 
+
+
+
   productController(req  , res){
 
     // creating the path of the current dir using the path module 
--- Project/MVC-Inventory-Management/src/module/produt.modal.js
+++ Project/MVC-Inventory-Management/src/module/produt.modal.js
@@ -20,8 +20,20 @@ export default class productModule{
 
   // creating the function of the get using the static method
   static get(){
-    
+        
+    // hear we are the return the get method 
+    return product ; 
   } 
 
 }
+
+
+// creating the array of the product 
+var product = [
+    new productModule(1 , 'product1' ,"fist product" , 37.5 , "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg" ),
+    new productModule(2 , 'product2' ,"second product" , 34.5 , "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg" ),
+    new productModule(3 , 'product3' ,"third product" , 30.5 , "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg" ),
+    new productModule(4 , 'product4' ,"fourth product" , 56.5 , "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg" )
+    
+]
 
