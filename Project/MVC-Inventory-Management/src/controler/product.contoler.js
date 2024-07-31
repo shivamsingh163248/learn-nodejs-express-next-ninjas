970d8872445a1d931c03a212073b0b8a06cf06f9
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-07-31 16:29:21 +0530

    update

--- Project/MVC-Inventory-Management/src/controler/product.contoler.js
+++ Project/MVC-Inventory-Management/src/controler/product.contoler.js
@@ -1,7 +1,7 @@
 // creating the object of the controller 
 
 import path from "path";
-import productModule from "../module/produt.modal";
+import productModule from "../module/produt.modal.js";
 
 // creating the class 
 
@@ -9,10 +9,15 @@ import productModule from "../module/produt.modal";
 
 // creating the function of th product controller
 // using the static method 
+// creating the product module 
 
 
 
   productController(req  , res){
+
+    let product = productModule.get() ; 
+    // now console product 
+    console.log(product) ; 
 
     // creating the path of the current dir using the path module 
    return  res.sendFile( path.join(path.resolve() , 'Project' , 'MVC-Inventory-Management' ,'src','view','products.html')) ; 
