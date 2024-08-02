415d64c90312f96fd70faaab4ba84874186dd9c4
SHIVAM SINGH <92671520+shivamsingh163248@users.noreply.github.com>
2024-08-02 19:53:05 +0530

    learning the ejs

--- Project/MVC-Inventory-Management/index.js
+++ Project/MVC-Inventory-Management/index.js
@@ -7,6 +7,10 @@ import product from './src/controler/product.contoler.js';
 // creating the server 
 const server = express() ; 
 
+// now hear we are the defining the ejs 
+server.set('view engine'  , "ejs") ; 
+server.set('view' ,  path.join(path.resolve() , 'Project' , 'MVC-Inventory-Management' ,'src','view')) ; 
+
 // now creating the server using the get 
 server.use(express.static('./Project/MVC-Inventory-Management/src/view')) ; 
 
